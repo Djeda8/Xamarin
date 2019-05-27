f9e18e1b97861b8ad43fea3ae1a1bfe25cd5716d
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-05-27 12:34:12 +0200

    initial commit

---
+++ BasicDebug/BasicDebug/BasicDebug/Views/Main/MainView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BasicDebug.Views.Main
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        Button debugButton;
+        public MainView()
+        {
+            InitializeComponent();
+            debugButton = new Button { Text = "Debug Button" };
+           
+
+            debugButton.Clicked += (sender, e) =>
+            {
+                Debug.WriteLine("You has pressed the debug button.");
+            };
+
+            Content = new StackLayout
+            {
+                Margin = new Thickness(20),
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "Basic Debug",
+                        FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                        FontAttributes = FontAttributes.Bold
+                    },
+                    new Label { Text = "Press the button and look the output window." },
+                    debugButton,
+                }
+            };
+        }
+    }
+}
---
+++ BasicDebug/Readme.md
@@ -0,0 +1,8 @@
+## Basic Debug
+
+You only must to add:
+
+`Debug.WriteLine("You has pressed the debug button.");
+
+And you'll see the text in the output window.
+
