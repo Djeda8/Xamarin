c365a2de7cf2712b304d4c99cedf49efac166335
Djeda8 <43439678+Djeda8@users.noreply.github.com>
2019-06-12 21:52:49 +0200

    inital commit

---
+++ AttachedBehavior1/AttachedBehavior1/AttachedBehavior1/Views/Main/MainView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AttachedBehavior1.ViewModels.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AttachedBehavior1.Views.Main
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ Behavior1/Behavior1/Behavior1/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Behavior1.Views.Main;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Behavior1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainView();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
