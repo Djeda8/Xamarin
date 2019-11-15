63dd7b51390d6068dd77d8880f661358b76179e7
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-11-15 12:13:59 +0100

    Binding initial

---
+++ Binding/Binding/Binding/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binding.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public string Modelo { get; set; }
+        public string Fabricante { get; set; }
+
+    }
+}
