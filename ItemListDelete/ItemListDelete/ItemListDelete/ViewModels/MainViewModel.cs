0156c6f97ee7894a943bcf5877ef4e808bc1ccf2
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-11-18 12:46:42 +0100

    ItemListDelete Initial

---
+++ ItemListDelete/ItemListDelete/ItemListDelete/ViewModels/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using ItemListDelete.Models;
+using ItemListDelete.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ItemListDelete.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public int Contador;
+
+        private ObservableCollection<Line> _lines;
+
+        public ObservableCollection<Line> Lines
+        {
+            get { return _lines; }
+            set
+            {
+                _lines = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Contador = 1;
+            Lines = new ObservableCollection<Line>();
+        }
+
+        public DelegateCommand AddCommand => new DelegateCommand(AddCommandExecute);
+
+        private void AddCommandExecute()
+        {
+            Line line = new Line()
+            {
+                Id = Contador,
+                Name = String.Format($"Line {Contador}"),
+            };
+
+            Lines.Add(line);
+
+            Contador++;
+        }
+
+        public DelegateCommand MinusCommand => new DelegateCommand(MinusCommandExecute);
+
+        private void MinusCommandExecute()
+        {
+            Lines.RemoveAt(Lines.Count-1);
+            Contador--;
+        }
+
+        public DelegateCommand<Line> DelNotifyCommand => new DelegateCommand<Line>(DelNotifyCommandExecute);
+
+        private void DelNotifyCommandExecute(Line line)
+        {
+            Lines.Remove(line);
+        }
+    }
+}
---
+++ ItemListDelete/ItemListDelete/ItemListDelete/Views/ItemViewTemplate.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ItemListDelete.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemViewTemplate : ContentView
+    {
+        public ItemViewTemplate()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ParentContextProperty =
+           BindableProperty.Create("ParentContext", typeof(object), typeof(ItemViewTemplate), null, propertyChanged: OnParentContextPropertyChanged);
+
+        public object ParentContext
+        {
+            get { return GetValue(ParentContextProperty); }
+            set { SetValue(ParentContextProperty, value); }
+        }
+
+        private static void OnParentContextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (newValue != oldValue && newValue != null)
+            {
+                (bindable as ItemViewTemplate).ParentContext = newValue;
+            }
+        }
+    }
+}
