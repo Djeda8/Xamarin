47c57061c09f74a0ce197b87da13f4a33421a6ae
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-11-19 12:54:21 +0100

    initial commit

---
+++ RoleFilter/RoleFilter/RoleFilter/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoleFilter.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get => ($"{Text} - {Role}"); }
+
+        public string Role { get; set; }
+    }
+}
---
+++ RoleFilter/RoleFilter/RoleFilter/Services/DataStore/DataStoreService.cs
@@ -0,0 +1,31 @@
+﻿using RoleFilter.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleFilter.Services.DataStore
+{
+    public class DataStoreService : IDataStoreService
+    {
+        public IList<Item> mockItems;
+
+        public DataStoreService()
+        {
+            mockItems = new List<Item>()
+            {
+                new Item { Text = "First item", Role = "Admin" },
+                new Item { Text = "Second item", Role="Admin" },
+                new Item { Text = "Third item", Role="Editor" },
+                new Item { Text = "Fourth item", Role="Editor" },
+                new Item { Text = "Fifth item",  Role="Student" },
+                new Item { Text = "Sixth item",  Role="Student" },
+            };
+
+        }
+        public async Task<IList<Item>> GetItemsAsync()
+        {
+            return mockItems;
+        }
+    }
+}
