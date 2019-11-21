55a47e21137a79be9ba3ebd05ccb42f44fb68a7b
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-11-21 12:24:50 +0100

    Initial commit

---
+++ SearchBarMVVM/SearchBarMVVM/SearchBarMVVM/ViewModels/SearchBarViewModel.cs
@@ -0,0 +1,38 @@
+﻿using SearchBarMVVM.Services.DataStore;
+using SearchBarMVVM.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SearchBarMVVM.ViewModels
+{
+    class SearchBarViewModel : ViewModelBase
+    {
+        public ICommand PerformSearch => new Command<string>((string query) =>
+        {
+            SearchResults = DataStoreService.GetSearchResults(query);
+        });
+
+        List<string> searchResults = DataStoreService.Sports;
+
+        public List<string> SearchResults
+        {
+            get
+            {
+                return searchResults;
+            }
+            set
+            {
+                searchResults = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public void PerformSearchRealtime(string newTextValue)
+        {
+            SearchResults = DataStoreService.GetSearchResults(newTextValue);
+        }
+    }
+}
